41e31f688fd329ce46e86c86e275c9f3de2dd77b
PatchOfScotland <d.marchant@ed-alumni.net>
2019-01-03 10:55:47 +0100

    initial commit to master

---
+++ walk_test.py
@@ -0,0 +1,11 @@
+
+import os
+for root, dirs, files in os.walk("/home/patch_of_scotland/Documents/Code/PyCharmProjects/MiG_based_prototype/auld_lang_syne", topdown=False):
+    for name in files:
+        print(os.path.join(root, name))
+
+print('')
+
+for root, dirs, files in os.walk("/home/patch_of_scotland/Documents/Code/PyCharmProjects/MiG_based_prototype/auld_lang_syne", topdown=False):
+    for name in files:
+        print(os.path.join(root, name))
